0750f20e28382177837755bb6b62e77078708d53
Vincent Bakker <vince2k@gmail.com>
2016-07-11 22:45:34 +0200

    Removed MQTT, bugfix allLights. TODO: switch to REST

--- scripts/allLights.py
+++ scripts/allLights.py
@@ -1,4 +1,5 @@
 from org.openhab.core.library.types import PercentType
+from org.openhab.core.library.types import OnOffType
 
 class AllLightsRule(Rule):
     def getEventTrigger(self):
@@ -9,19 +10,27 @@ class AllLightsRule(Rule):
         ]
 
     def execute(self, event):
-        print("Execute")
-        items = filter(lambda x: isinstance(x.state, PercentType), ir.getItems("GF_Living_Lamp*"))
-        print(items)
-        allLivingOn = all(map(lambda x: int(str(x.state)) > 0, items))
-        currentState = int(str(ir.getItem("GF_All_lights").state))
-        #kitchenOn = int(str(ir.getItem("GF_Kitchen_Lamp").state)) > 0
-        print("allLiving %s kitchen %s currentState %s" % (allLivingOn, currentState))
-        if allLivingOn and currentState != 1:
-             oh.postUpdate("GF_All_lights","1")
-        elif not allLivingOn and currentState != 0:
-             oh.postUpdate("GF_All_lights","0")
-        else:
-             oh.postUpdate("GF_All_lights","2")
+        try:
+            oh.logInfo("AllLights","Execute")
+            dimmer_items = filter(lambda x: isinstance(x.state, PercentType), ir.getItems("GF_Living_Lamp*"))
+            switch_items = filter(lambda x: isinstance(x.state,OnOffType), ir.getItems("GF_Living_Lamp*"))
+            oh.logInfo("AllLights","Dimmer {} OnOff ", [dimmer_items, switch_items])
+            allDimmerOn = all(map(lambda x: int(str(x.state)) > 0, dimmer_items))
+            allSwitchOn = all(map(lambda x: int(str(x.state)) > 0, switch_items))
+            currentState = int(str(ir.getItem("GF_All_lights").state))
+            #kitchenOn = int(str(ir.getItem("GF_Kitchen_Lamp").state)) > 0
+            oh.logInfo("AllLights","allDimmerOn {} allSwitchOn {}", [allDimmerOn, allSwitchOn])
+            #print("allLiving %s kitchen %s currentState %s" % (allLivingOn, currentState))
+
+            if allDimmerOn and allSwitchOn and currentState != 1:
+                 oh.postUpdate("GF_All_lights","1")
+            elif not allDimmerOn and not allSwitchOn and currentState != 0:
+                 oh.postUpdate("GF_All_lights","0")
+            else:
+                 oh.postUpdate("GF_All_lights","2")
+        except Exception as e:
+            oh.logError("AllLights", "Something went wrong!")
+            oh.logError("AllLights", "Test {}", e)
 
 
 def getRules():
